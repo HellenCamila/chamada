99975733706c2ae087df5d90a918ac6e5c20e5a8
HellenCamila <132139100+HellenCamila@users.noreply.github.com>
2023-06-17 11:53:14 -0300

    Add files via upload

---
+++ atividade1/src/main/java/sp/senai/br/atividade1/aluno.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp.senai.br.atividade1;
+
+/**
+ *
+ * @author aluno
+ */
+class aluno {
+
+    static int length;
+    
+}
